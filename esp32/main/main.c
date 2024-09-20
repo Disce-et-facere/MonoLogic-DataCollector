8568bf64cb23b6a018fc23928bdb1217683a3b05
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-09-20 12:47:41 +0200

    Added mqtt as a task and made it properly post it to a server

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -9,24 +9,31 @@
 #include "dht11.h"
 #include "esp_log.h"
 #include "freertos/idf_additions.h"
+#include "freertos/projdefs.h"
 #include "mqtt.h"
 #include "mqtt_client.h"
 #include "nvs_flash.h"
 #include "portmacro.h"
 #include "wifi.h"
+#include <stdlib.h>
 
-void DHT_task(void *pvParameter) {
-  dht_t dhtStruct;
-  dhtInit(&dhtStruct);
+dht_t *dhtStructPtr;
+
+void dhtTask(void *pvParameter) {
+  /*dht_t dhtStruct;*/
+  /*dhtInit(&dhtStruct);*/
+  dhtInit(dhtStructPtr);
+  dht_t *dhtStruct = dhtStructPtr;
   ESP_LOGI(DHTTAG, "Entering dht loop");
   while (true) {
-    dht_err_t dhtStatus = dhtRead(&dhtStruct);
+    /*dht_err_t dhtStatus = dhtRead(&dhtStruct);*/
+    dht_err_t dhtStatus = dhtRead(dhtStructPtr);
     switch (dhtStatus) {
     case DHT_OK:
       ESP_LOGI(DHTTAG, "read ok");
       ESP_LOGI(DHTTAG, "Temperature: %d.%d Humidity: %d.%d%%",
-               dhtStruct.temperature.integer, dhtStruct.temperature.decimal,
-               dhtStruct.humidity.integer, dhtStruct.humidity.decimal);
+               dhtStruct->temperature.integer, dhtStruct->temperature.decimal,
+               dhtStruct->humidity.integer, dhtStruct->humidity.decimal);
       break;
     case DHT_TIMEOUT_FAIL:
       ESP_LOGE(DHTTAG, "Dht timeout error");
@@ -45,6 +52,30 @@ void DHT_task(void *pvParameter) {
   }
 }
 
+void mqttTask(void *pvParameter) {
+  ESP_LOGI(WIFITAG, "ESP_WIFI_MODE_STA");
+  if (wifi_init_sta()) {
+    ESP_LOGI(WIFITAG, "Connected to wifi successfully");
+    ESP_LOGI(MQTTTAG, "MQTT starting");
+    esp_mqtt_client_handle_t mqttClient = mqtt_init();
+    ESP_LOGI(MQTTTAG, "MQTT started");
+    esp_mqtt_client_publish(mqttClient, "/idfpye/qos1", "publish", 0, 1, 0);
+    esp_mqtt_client_enqueue(mqttClient, "/idfpye/qos1", "enqueue", 0, 1, 0,
+                            false);
+    char buff[100];
+    while (true) {
+      sprintf(buff, "{Temperature:%d.%d Humidity:%d.%d}",
+              dhtStructPtr->temperature.integer,
+              dhtStructPtr->temperature.decimal, dhtStructPtr->humidity.integer,
+              dhtStructPtr->humidity.decimal);
+      esp_mqtt_client_enqueue(mqttClient, "/idfpye/qos1", buff, 0, 1, 0, false);
+      ESP_LOGI(MQTTTAG, "read temp %d.%d", dhtStructPtr->temperature.integer,
+               dhtStructPtr->temperature.decimal);
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+  }
+}
+
 void app_main(void) {
   // Initialize NVS
   esp_err_t ret = nvs_flash_init();
@@ -55,15 +86,20 @@ void app_main(void) {
   }
   ESP_ERROR_CHECK(ret);
 
-  ESP_LOGI(WIFITAG, "ESP_WIFI_MODE_STA");
-  if (wifi_init_sta()) {
-    ESP_LOGI(WIFITAG, "Connected to wifi successfully");
-    ESP_LOGI(MQTTTAG, "MQTT starting");
-    esp_mqtt_client_handle_t mqttClient = mqtt_init();
-    ESP_LOGI(MQTTTAG, "MQTT started");
-    esp_mqtt_client_publish(mqttClient, "/idfpye/qos1", "publish", 0, 1, 0);
-    esp_mqtt_client_enqueue(mqttClient, "/idfpye/qos1", "enqueue", 0, 1, 0,
-                            false);
+  dhtStructPtr = (dht_t *)malloc(sizeof(dht_t));
+
+  TaskHandle_t dhtHandle = NULL;
+  BaseType_t taskRet =
+      xTaskCreate(&dhtTask, "DHT task", 4096, NULL, 5, &dhtHandle);
+  if (taskRet == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
+    vTaskDelete(dhtHandle);
+    ESP_LOGE(DHTTAG, "Could not allocate memory for task");
   }
-  xTaskCreate(&DHT_task, "DHT task", 4096, NULL, 5, NULL);
+
+  TaskHandle_t mqttHandle = NULL;
+  taskRet = xTaskCreate(&mqttTask, "mqtt task", 4096, NULL, 5, &mqttHandle);
+  if (taskRet == errCOULD_NOT_ALLOCATE_REQUIRED_MEMORY) {
+    vTaskDelete(mqttHandle);
+    ESP_LOGE(MQTTTAG, "Could not allocate memory for task");
+  }
 }
