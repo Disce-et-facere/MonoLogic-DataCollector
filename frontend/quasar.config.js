6f0959c6572ded04f046ee7432cf072244aee387
Simon Thorell <kontakt@simonthorell.se>
2024-09-23 20:43:56 +0200

    Fix main

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -84,6 +84,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
+      port: 8080,
       // https: true
       open: true // opens browser window automatically
     },
