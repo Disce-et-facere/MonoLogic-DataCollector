fbde8da9d692e4beb495827243d26b7016c4126a
Emil K <29043737+Cosmao@users.noreply.github.com>
2024-09-24 11:44:46 +0200

    Removed unused func

--- esp32/main/https.c
+++ esp32/main/https.c
@@ -118,44 +118,6 @@ esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
   return ESP_OK;
 }
 
-void https_with_url(void) {
-#define buffSize 128
-  uint8_t mac[6];
-  esp_wifi_get_mac(ESP_IF_WIFI_STA, mac);
-  char url[buffSize];
-  snprintf(url, buffSize,
-           "https://www.skippings.com/api/sensor-data/"
-           "%0x2:%0x2:%0x2:%0x2:%0x2:%0x2)",
-           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-  esp_http_client_config_t config = {
-      .url = url,
-      .event_handler = _http_event_handler,
-      .skip_cert_common_name_check = true,
-      .transport_type = HTTP_TRANSPORT_OVER_SSL,
-  };
-  esp_http_client_handle_t client = esp_http_client_init(&config);
-  esp_http_client_set_method(client, HTTP_METHOD_GET);
-
-  const char *testPost = "{\"temperature\":100,\"humidity\":0}";
-  /*esp_http_client_set_url(*/
-  /*client, "https://www.skippings.com/api/sensor-data/A1:B2:C3:D4:E5");*/
-  esp_http_client_set_method(client, HTTP_METHOD_POST);
-  esp_http_client_set_header(client, "Content-Type", "application/json");
-  esp_http_client_set_post_field(client, testPost, strlen(testPost));
-
-  esp_err_t err = esp_http_client_perform(client);
-
-  if (err == ESP_OK) {
-    ESP_LOGI(HTTPTAG, "HTTPS Status = %d, content_length = %" PRId64,
-             esp_http_client_get_status_code(client),
-             esp_http_client_get_content_length(client));
-  } else {
-    ESP_LOGE(HTTPTAG, "Error perform http request %s", esp_err_to_name(err));
-  }
-  esp_http_client_cleanup(client);
-}
-
 void httpsTask(void *pvParameter) {
 #define buffSize 128
   if (wifi_init_sta()) {
@@ -183,9 +145,6 @@ void httpsTask(void *pvParameter) {
         esp_http_client_handle_t client = esp_http_client_init(&config);
         esp_http_client_set_method(client, HTTP_METHOD_POST);
         esp_http_client_set_header(client, "Content-Type", "application/json");
-        // TODO: Auth my MAC, overriding now
-        /*esp_http_client_set_url(*/
-        /*client, "https://www.skippings.com/api/sensor-data/A1:B2:C3:D4:E5");*/
 
         char post[buffSize];
         snprintf(post, buffSize, "{\"temperature\":%1.f,\"humidity\":%1.f}",
--- esp32/main/https.h
+++ esp32/main/https.h
@@ -1,7 +1,6 @@
 #ifndef __https_h
 #define __https_h
 
-void https_with_url(void);
 void httpsTask(void *pvParameter);
 void httpsAuthenticate(void);
 
